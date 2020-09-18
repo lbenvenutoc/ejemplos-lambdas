155f9d014913647e363a4158484395c6b314a8b5
lbenvenutoc <lbenvenutoc@gmail.com>
2020-09-18 15:11:53 -0500

    Se agrego bean

---
+++ src/main/java/com/lbenvenuto/PersonaDto.java
@@ -0,0 +1,66 @@
+package com.lbenvenuto;
+
+public class PersonaDto {
+
+	private String id;
+	private String dni;
+	private String apellidoPaterno;
+	private String apellidoMaterno;
+	private String nombre;
+	private String edad;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getApellidoPaterno() {
+		return apellidoPaterno;
+	}
+
+	public void setApellidoPaterno(String apellidoPaterno) {
+		this.apellidoPaterno = apellidoPaterno;
+	}
+
+	public String getApellidoMaterno() {
+		return apellidoMaterno;
+	}
+
+	public void setApellidoMaterno(String apellidoMaterno) {
+		this.apellidoMaterno = apellidoMaterno;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getEdad() {
+		return edad;
+	}
+
+	public void setEdad(String edad) {
+		this.edad = edad;
+	}
+
+	@Override
+	public String toString() {
+		return "PersonaDto [id=" + id + ", dni=" + dni + ", apellidoPaterno=" + apellidoPaterno + ", apellidoMaterno="
+				+ apellidoMaterno + ", nombre=" + nombre + ", edad=" + edad + "]";
+	}
+
+}
